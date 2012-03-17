0c3c1130cc54c725a2906efc7b038aa6e7d49e87
Leonard Garvey <lengarvey@bottledup.net>
2012-03-18 01:41:14 +1100

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blitter::Application.config.secret_token = '8dbd88e6bf91a80061a29176836d40be5411c97d1a9e569de6df58bf7a614d95a9e40b2da40428559ca2863f60940212068a8c0071d78c830c30a357cb824877'
